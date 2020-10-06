3ce1ce06d97b573327014f4ce48155ffd875c5d8
manavendrasen <manavendra.199302058@muj.manipal.edu>
2020-10-06 23:32:33 +0530

    Added Quote and download feature

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
-import React from "react";
+import { exportComponentAsJPEG } from "react-component-export-image";
+import React, { useRef } from "react";
 import "./App.css";
+import { Container, makeStyles, Button } from "@material-ui/core";
 import { Quote } from "./components/Quote";
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    margin: "1rem auto",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alighItems: "center",
+  },
+
+  btn: {
+    margin: "1rem auto",
+    width: "200px",
+  },
+}));
+
+const ComponentToPrint = React.forwardRef((props, ref) => (
+  <div
+    style={{ margin: "auto", width: "min-content", height: "min-content" }}
+    ref={ref}
+  >
+    <Quote />
+  </div>
+));
+
 function App() {
+  const componentRef = useRef();
+
+  const classes = useStyles();
   return (
-    <>
-      <Quote />
-    </>
+    <React.Fragment>
+      <Container className={classes.container}>
+        <ComponentToPrint ref={componentRef} />
+        <Button
+          className={classes.btn}
+          variant="contained"
+          color="secondary"
+          onClick={() => exportComponentAsJPEG(componentRef)}
+        >
+          Export As JPEG
+        </Button>
+      </Container>
+    </React.Fragment>
   );
 }
 
--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -1,37 +1,93 @@
 import React, { useState, useEffect } from "react";
-import { Typography, Box, Card } from "@material-ui/core";
+import { Typography, Box, Card, makeStyles } from "@material-ui/core";
 import { URL } from "../constants/routes";
 import axios from "axios";
 
+const useStyles = makeStyles((theme) => ({
+  cardCont: {
+    margin: "auto",
+    backgroundColor: "#0110f9",
+    padding: "3rem",
+    width: "min-content",
+  },
+  card: {
+    position: "relative",
+
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "flex-start",
+    flexDirection: "column",
+    width: "500px",
+    height: "500px",
+    // height: "min-content",
+    borderRadius: "8px",
+    boxShadow: "0px 0px 20px #010a94",
+    padding: "2rem",
+  },
+  quote: {
+    fontSize: "1.25rem",
+    fontFamily: "Poppins",
+    fontWeight: "700",
+  },
+  author: {
+    marginTop: "1.5rem",
+    fontSize: "1rem",
+    fontFamily: "Poppins",
+    fontWeight: "600",
+  },
+  leftbottom: {
+    position: "absolute",
+    bottom: "2rem",
+    fontWeight: "700",
+    fontFamily: "Poppins",
+    left: "2rem",
+    color: "#0110f9",
+  },
+  lefttop: {
+    position: "absolute",
+    top: "2rem",
+    fontWeight: "700",
+    fontFamily: "Poppins",
+    left: "2rem",
+    color: "#ff007b",
+    "&:before": {
+      content: "",
+      width: "100%",
+      height: "10px",
+      backgroundColor: "#ff007b",
+    },
+  },
+}));
+
 export const Quote = () => {
+  const classes = useStyles();
   const [quote, setQuote] = useState("");
   const [author, setAuthor] = useState("");
+  const [category, setCategory] = useState("");
   useEffect(() => {
     const fetchQuote = async () => {
       const res = await axios.get(URL);
       //   const result = res.data;
-      const { quote, author } = res.data.contents.quotes[0];
+      const { quote, author, category } = res.data.contents.quotes[0];
+      //   console.log(res.data.contents.quotes[0]);
       setAuthor(author);
       setQuote(quote);
+      setCategory(category);
     };
     fetchQuote();
   }, []);
   return (
     <>
-      <Card
-        style={{
-          borderRadius: "8px",
-          border: "none",
-          boxShadow: "0px 0px 20px #cccccc",
-          padding: "2rem",
-          margin: "2rem",
-        }}
-      >
-        <Typography style={{ fontFamily: "Gilroy", fontWeight: "900" }}>
-          {quote}
-        </Typography>
-        <Typography>{author}</Typography>
-      </Card>
+      <Box className={classes.cardCont}>
+        <Card className={classes.card}>
+          <Typography className={classes.lefttop}>{category}</Typography>
+          <Typography className={classes.leftbottom}>
+            Follow @quotipy for more
+          </Typography>
+          <Typography className={classes.quote}>{quote}</Typography>
+          <Typography className={classes.author}>{author}</Typography>
+        </Card>
+      </Box>
     </>
   );
 };
